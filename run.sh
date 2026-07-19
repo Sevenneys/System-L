c14712aeee533415b0c55a0726164879c882b1d4
sevenneys <sevenneys@gmail.com>
2026-07-19 12:39:34 +0300

    Main init run.sh v1

---
+++ run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+source /etc/os-release
+path=$(pwd)
+
+
+access_control() {
+    clear
+    sudo -v || access_denided
+
+    if [ ! -d /home/$USER/packages ]; then
+        mkdir /home/$USER/packages
+
+        sudo chmod -R 700 /home/$USER/packages
+        sudo chown $USER:$USER /home/$USER/packages
+        sudo chmod -R 700 $path/modules
+        sudo chown $USER:$USER $path/modules
+
+    fi
+
+    run_proccess
+}
+
+
+access_denided(){
+    echo ""
+    echo "WARNING: Access denided!"
+    echo
+    exit
+}
+
+
+run_proccess() {
+
+    case $ID in
+        "ubuntu")
+        sudo ./modules/ubuntu.sh
+    ;;
+        "debian")
+        sudo ./modules/debian.sh
+    ;;
+        "kali")
+        sudo ./modules/kali.sh
+    ;;
+        *)
+        echo "ERROR: Unsupported system.."
+    ;;
+    esac
+
+}
+
+
+access_control
